beccef868c1909bbf4de9a8a62a595f9278dcdf6
come25136 <homebox25136@gmail.com>
2015-12-10 22:11:42 +0900

    Initial commit

---
+++ cmd for background/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace cmd_for_background
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.filepass = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.run = new System.Windows.Forms.Button();
+            this.workspece = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cw = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exit_ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // filepass
+            // 
+            this.filepass.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.filepass.BackColor = System.Drawing.SystemColors.Window;
+            this.filepass.Location = new System.Drawing.Point(84, 12);
+            this.filepass.MaxLength = 99999;
+            this.filepass.Name = "filepass";
+            this.filepass.Size = new System.Drawing.Size(391, 19);
+            this.filepass.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(482, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "参照";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // run
+            // 
+            this.run.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.run.Location = new System.Drawing.Point(544, 12);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(84, 44);
+            this.run.TabIndex = 2;
+            this.run.Text = "実行";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // workspece
+            // 
+            this.workspece.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.workspece.BackColor = System.Drawing.SystemColors.Window;
+            this.workspece.Location = new System.Drawing.Point(84, 37);
+            this.workspece.MaxLength = 99999;
+            this.workspece.Name = "workspece";
+            this.workspece.Size = new System.Drawing.Size(391, 19);
+            this.workspece.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(482, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 19);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "参照";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(23, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ファイル:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "作業フォルダ:";
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.Controls.Add(this.cw);
+            this.panel1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.panel1.Location = new System.Drawing.Point(0, 62);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(640, 372);
+            this.panel1.TabIndex = 10;
+            // 
+            // cw
+            // 
+            this.cw.AutoSize = true;
+            this.cw.BackColor = System.Drawing.Color.Transparent;
+            this.cw.ForeColor = System.Drawing.Color.White;
+            this.cw.Location = new System.Drawing.Point(0, 0);
+            this.cw.Name = "cw";
+            this.cw.Size = new System.Drawing.Size(0, 12);
+            this.cw.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(640, 373);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "cmbがバックグラウンドで動作しています。";
+            this.notifyIcon1.BalloonTipTitle = "cmb";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "cmd for background";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exit_ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(153, 48);
+            // 
+            // exit_ToolStripMenuItem
+            // 
+            this.exit_ToolStripMenuItem.Name = "exit_ToolStripMenuItem";
+            this.exit_ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exit_ToolStripMenuItem.Text = "終了";
+            this.exit_ToolStripMenuItem.Click += new System.EventHandler(this.exit_ToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(640, 434);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.run);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.workspece);
+            this.Controls.Add(this.filepass);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "cmd for background pronama edition";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox filepass;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.TextBox workspece;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label cw;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exit_ToolStripMenuItem;
+    }
+}
+
---
+++ cmd for background/Form1.cs
@@ -0,0 +1,205 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace cmd_for_background
+{
+    public partial class Form1 : Form
+    {
+        string filename;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cfbpe.Properties.Settings.Default.Reload();
+            filepass.Text = cfbpe.Properties.Settings.Default.filepass;
+            workspece.Text = cfbpe.Properties.Settings.Default.workspece;
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            panel1.Parent = pictureBox1;
+            panel1.Location = new Point(0, 0);
+        }
+
+        //ファイル or コマンド
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //OpenFileDialogクラスのインスタンスを作成
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //「ファイル名」で表示される文字列を指定する
+            ofd.FileName = filename;
+            //[ファイルの種類]に表示される選択肢を指定する
+            ofd.Filter = "バッチファイル(.bat .cmd)|*.bat;*.cmd|すべてのファイル(*.*)|*.*";
+            //「すべてのファイル」が選択されているようにする
+            ofd.FilterIndex = 1;
+            //タイトルを設定する
+            ofd.Title = "ファイルを選択してください";
+            //ダイアログボックスを閉じる前に現在のディレクトリを復元するようにする
+            ofd.RestoreDirectory = true;
+
+
+            //ダイアログを表示する
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                filepass.Text = ofd.FileName;
+                filename = Path.GetFileName(@ofd.FileName);
+                cfbpe.Properties.Settings.Default.filepass = ofd.FileName;
+            }
+            else
+            {
+                //メッセージボックスを表示する
+                MessageBox.Show("ファイルが選択されていません",
+                    "cmd for background",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        //作業フォルダ
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var Dialog = new CommonOpenFileDialog();
+            // フォルダーを開く設定に
+            Dialog.IsFolderPicker = true;
+            // 読み取り専用フォルダ/コントロールパネルは開かない
+            Dialog.EnsureReadOnly = false;
+            Dialog.AllowNonFileSystemItems = false;
+            // パス指定
+            Dialog.DefaultDirectory = Application.StartupPath;
+            // 開く
+            var Result = Dialog.ShowDialog();
+            // もし開かれているなら
+            if (Result == CommonFileDialogResult.Ok)
+            {
+                // ここでいろいろする（開いたフォルダはDialog.FileNameで取得）
+                Directory.SetCurrentDirectory(Dialog.FileName);
+                workspece.Text = Directory.GetCurrentDirectory();
+                cfbpe.Properties.Settings.Default.workspece = Directory.GetCurrentDirectory();
+            }
+            else
+            {
+                //メッセージボックスを表示する
+                MessageBox.Show("作業フォルダが選択されていません",
+                    "cmd for background",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+        }
+
+        //実行
+        public void run_Click(object sender, EventArgs e)
+        {
+            if (filepass.Text == null)
+            {
+                MessageBox.Show("バッチファイルが選択されていません",
+                    "cmd for background",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+            }
+            else
+            {
+                //Processオブジェクトを作成
+                Process p = new System.Diagnostics.Process();
+
+                //出力をストリームに書き込むようにする
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.RedirectStandardError = true;
+                //OutputDataReceivedイベントハンドラを追加
+                p.OutputDataReceived += p_OutputDataReceived;
+
+                p.StartInfo.FileName = System.Environment.GetEnvironmentVariable("ComSpec");
+                p.StartInfo.RedirectStandardInput = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.Arguments = @"/c " + filepass.Text;
+                //起動
+                p.Start();
+
+                //非同期で出力の読み取りを開始
+                p.BeginOutputReadLine();
+            }
+        }
+
+        //OutputDataReceivedイベントハンドラ
+        //行が出力されるたびに呼び出される
+        private void p_OutputDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            //出力された文字列を表示する
+            Console.WriteLine(e.Data);
+
+            // Delegateを作成
+            Action act = () =>
+            {
+                if (string.IsNullOrEmpty(e.Data) == false)
+                {
+                    cw.Text += (e.Data);
+                }
+
+                cw.Text += (Environment.NewLine);
+            };
+
+            BeginInvoke(act);
+        }
+
+        //ErrorDataReceivedイベントハンドラ
+        //行が出力されるたびに呼び出される
+        private void p_ErrorDataReceived(object sender, System.Diagnostics.DataReceivedEventArgs e)
+        {
+            //出力された文字列を表示する
+            Console.WriteLine("ERR>{0}", e.Data);
+
+            // Delegateを作成
+            Action act = () =>
+            {
+                if (string.IsNullOrEmpty(e.Data) == false)
+                {
+                    cw.Text += (e.Data);
+                }
+
+                cw.Text += (Environment.NewLine);
+            };
+
+            BeginInvoke(act);
+        }
+
+        private void p_Exited(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process p = new Process();
+            p.Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason != CloseReason.ApplicationExitCall)
+            {
+                e.Cancel = true; // フォームが閉じるのをキャンセル
+                this.Visible = false; // フォームの非表示
+            }
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.Visible = true; // フォームの表示
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.WindowState = FormWindowState.Normal; // 最小化をやめる
+            }
+            this.Activate(); // フォームをアクティブにする
+        }
+
+        private void exit_ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.Visible = false; // アイコンをトレイから取り除く
+            Application.Exit(); // アプリケーションの終了
+        }
+    }
+}
